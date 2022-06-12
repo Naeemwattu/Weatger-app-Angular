5c3a7170983a4621ed30bb3761b6c8a9206060d4
naeemwattu <naeemwattu6@gmail.com>
2022-06-12 22:51:24 +0500

    2nd-day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainPageComponent } from './main-page/main-page.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { LeftContentComponent } from './main-page/left-content/left-content.component';
+import { RightContentComponent } from './main-page/right-content/right-content.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
+    LeftContentComponent,
+    RightContentComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-page/left-content/left-content.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-content',
+  templateUrl: './left-content.component.html',
+  styleUrls: ['./left-content.component.css']
+})
+export class LeftContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
+  list:any []=[];
+  addWeather(item:string){
+    this.list.push({id:this.list.length, name:item});
+    console.warn(this.list);
+  }
+
+}
